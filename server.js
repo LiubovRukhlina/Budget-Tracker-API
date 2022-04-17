41cb62d93e4db5767e9567f0a7053ffa193d4c3f
LR <liuba.rukhlina@gmail.com>
2022-04-18 00:09:39 +0200

    CORS config

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import Fastify from "fastify";
+import fastifyCors from "fastify-cors";
+
 import expenseRoutes from "./routes/expenses.js";
 
 const PORT = process.env.PORT || 5000;
 
 const fastify = Fastify({
   logger: true,
+});
+
+fastify.register(fastifyCors, (instance) => (req, callback) => {
+  let corsOptions = { origin: "*" };
+
+  callback(null, corsOptions); // callback expects two parameters: error and options
 });
 
 fastify.register(expenseRoutes);
